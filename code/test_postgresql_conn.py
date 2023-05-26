fe78e163fcfae7de0089e1fe92ad3231dfba6c1c
Daria <daria.liutina@aalto.fi>
2023-05-27 00:27:32 +0300

    queries kindaof continued

--- code/test_postgresql_conn.py
+++ code/test_postgresql_conn.py
@@ -6,61 +6,69 @@ import pandas as pd
 from sqlalchemy import create_engine, text
 
 try:
-   # Get the directory path of the current script
-   script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Get the directory path of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
 
-   # Remove the "/code" part from the script directory path
-   parent_dir = os.path.dirname(script_dir)
+    # Remove the "/code" part from the script directory path
+    parent_dir = os.path.dirname(script_dir)
 
-   # Construct the relative file path
-   file_path = os.path.join(parent_dir, 'data', 'vaccine-distribution-data.xlsx')
+    # Construct the relative file path
+    file_path = os.path.join(parent_dir, 'data', 'vaccine-distribution-data.xlsx')
 
-   # Read the Excel file into a pandas DataFrame
-   df = pd.ExcelFile(file_path)
+    # Read the Excel file into a pandas DataFrame
+    df = pd.ExcelFile(file_path)
 
-   # Use the DataFrame for further processing or analysis
-   sheet_names=df.sheet_names
-   data={}
-   for sheet in sheet_names:
-      temp=df.parse(sheet)
-      temp.columns = temp.columns.str.lower()
-      data[sheet]=temp
-   tables={}
-   tables['vaccinetype']='VaccineType'
-   tables['manufacturer']='Manufacturer'
+    # Use the DataFrame for further processing or analysis
+    sheet_names = df.sheet_names
+    data = {}
+    for sheet in sheet_names:
+        temp = df.parse(sheet)
+        temp.columns = temp.columns.str.lower()
+        data[sheet] = temp
 
+    # relating the SQL tables to the imported sheets in the dataframe
+    tables = {}
+    tables['vaccinetype'] = 'VaccineType'
+    tables['manufacturer'] = 'Manufacturer'
+    tables['vaccinationstations'] = 'VaccinationStations'
+    tables['vaccinebatch'] = 'VaccineBatch'
+    tables['transportationlog'] = 'Transportation log'
+    tables['staffmembers'] = 'StaffMembers'
+    tables['shifts'] = 'Shifts'
+    tables['vaccinations'] = 'Vaccinations'
+    tables['patients'] = 'Patients'
+    tables['vaccinepatients'] = 'VaccinePatients'
+    tables['symptoms'] = 'Symptoms'
+    tables['diagnosis'] = 'Diagnosis'
 
-   # Connect to an test database 
-   # NOTE: 
-   # 1. NEVER store credential like this in practice. This is only for testing purpose
-   # 2. Replace your "database" name, "user" and "password" that we provide to test the connection to your database 
-   database = 'grp02db_2023'  # TO BE REPLACED
-   user = 'grp02_2023'  # TO BE REPLACED
-   password = 'A88z1c6B'  # TO BE REPLACED
-   host = 'dbcourse.cs.aalto.fi'
+    # Connect to the database
+    database = 'grp02db_2023'  # TO BE REPLACED
+    user = 'grp02_2023'  # TO BE REPLACED
+    password = 'A88z1c6B'  # TO BE REPLACED
+    host = 'dbcourse.cs.aalto.fi'
 
-   connection = psycopg2.connect(
-      database="grp02db_2023",  # TO BE REPLACED
-      user='grp02_2023',  # TO BE REPLACED
-      password='A88z1c6B',  # TO BE REPLACED
-      host='dbcourse.cs.aalto.fi',
-      port='5432'
-   )
-   connection.autocommit = True
+    connection = psycopg2.connect(
+        database="grp02db_2023",  # TO BE REPLACED
+        user='grp02_2023',  # TO BE REPLACED
+        password='A88z1c6B',  # TO BE REPLACED
+        host='dbcourse.cs.aalto.fi',
+        port='5432'
+    )
+    connection.autocommit = True
 
-   DIALECT = 'postgresql+psycopg2://'
-   db_uri = "%s:%s@%s/%s" % (user, password, host, database)
-   engine = create_engine(DIALECT + db_uri)
-   psql_conn = engine.connect()
+    DIALECT = 'postgresql+psycopg2://'
+    db_uri = "%s:%s@%s/%s" % (user, password, host, database)
+    engine = create_engine(DIALECT + db_uri)
+    psql_conn = engine.connect()
 
-   for key in tables:
-      print(data[tables[key]])
-      data[tables[key]].to_sql(key, con=psql_conn, if_exists='append', index=False)
+    for key in tables:
+        print(data[tables[key]])
+        data[tables[key]].to_sql(key, con=psql_conn, if_exists='append', index=False)
 
 except (Exception, Error) as error:
-   print("Error while connecting to PostgreSQL", error)
+    print("Error while connecting to PostgreSQL", error)
 finally:
-   if (connection):
-      psql_conn.close()
-      connection.close()
-      print("PostgreSQL connection is closed")
+    if (connection):
+        psql_conn.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
